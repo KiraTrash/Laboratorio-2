dfb52b187840f6301626ce3bdbfb3f4abbf208e0
KiraTrash <146700374+KiraTrash@users.noreply.github.com>
2025-03-08 22:33:46 -0600

    Initial commit

---
+++ src/components/list.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <ul>
+        <li>Comida china</li>
+        <li>Tacos de birria</li>
+        <li>Carne molida</li>
+      </ul>
+    </div>
+  );
+}
+
+export default List;
